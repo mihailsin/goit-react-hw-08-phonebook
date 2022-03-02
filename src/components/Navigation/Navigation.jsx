c0a75eece3180ffcd8c81afbf6f26a823fc0b7c9
mihailsin <mihailsin88@gmail.com>
2022-03-03 00:25:46 +0200

    add navLinks styles

--- src/components/Navigation/Navigation.jsx
+++ src/components/Navigation/Navigation.jsx
@@ -1,15 +1,43 @@
 import React from 'react';
-import { Routes, Route, Link } from 'react-router-dom';
+import AppBar from '@mui/material/AppBar';
+import { Routes, Route, NavLink } from 'react-router-dom';
 
+const navLink = {
+  marginLeft: 20,
+  color: '#ffffff',
+  textDecoration: 'none',
+};
+const activeLink = {
+  marginLeft: 20,
+  color: '#000000',
+  textDecoration: 'underline',
+};
 const Navigation = () => {
   return (
-    <nav>
-      <ul>
-        <Link to="register">Register</Link>
-        <Link to="login">Login</Link>
-        <Link to="contacts">Contacts</Link>
-      </ul>
-    </nav>
+    <AppBar position="static">
+      <nav>
+        <ul>
+          <NavLink
+            style={({ isActive }) => (isActive ? activeLink : navLink)}
+            to="register"
+          >
+            Register
+          </NavLink>
+          <NavLink
+            style={({ isActive }) => (isActive ? activeLink : navLink)}
+            to="login"
+          >
+            Login
+          </NavLink>
+          <NavLink
+            style={({ isActive }) => (isActive ? activeLink : navLink)}
+            to="contacts"
+          >
+            Contacts
+          </NavLink>
+        </ul>
+      </nav>
+    </AppBar>
   );
 };
 
