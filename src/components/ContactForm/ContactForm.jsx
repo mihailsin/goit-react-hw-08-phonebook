1d5fb51c1ee64335fadd1a64309f3652b0b62b75
mihailsin <mihailsin88@gmail.com>
2022-03-12 17:54:28 +0200

    add required attributes in add & edit form-fields

--- src/components/ContactForm/ContactForm.jsx
+++ src/components/ContactForm/ContactForm.jsx
@@ -72,6 +72,7 @@ const ContactForm = () => {
           name="name"
           pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
           title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
+          required
         />
         <TextField
           variant="outlined"
@@ -83,6 +84,7 @@ const ContactForm = () => {
           name="number"
           pattern="\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}"
           title="Phone number must be digits and can contain spaces, dashes, parentheses and can start with +"
+          required
         />
         {isLoading ? (
           <SubmitButton
--- src/components/EditContactForm/EditContactForm.jsx
+++ src/components/EditContactForm/EditContactForm.jsx
@@ -118,6 +118,7 @@ const EditContactForm = ({
           name="name"
           pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
           title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
+          required
         />
         <TextField
           id={numberInputid}
@@ -128,6 +129,7 @@ const EditContactForm = ({
           name="number"
           pattern="\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}"
           title="Phone number must be digits and can contain spaces, dashes, parentheses and can start with +"
+          required
         />
         {isLoading ? (
           <SubmitButton
